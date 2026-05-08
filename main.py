a8cead434c5b0d0d6ee6a0c8ff3dfbe8d173c4c5
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-09 01:36:00 +0300

    Add archive call function

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src.services.call_services import (
     get_all_calls,
     get_call_by_id,
+    archive_call,
 )
 
 print(get_all_calls())
 
 print(get_call_by_id("3"))
+
+print(archive_call("3"))
--- src/services/call_services.py
+++ src/services/call_services.py
@@ -10,3 +10,14 @@ def get_call_by_id(call_id):
         if call["id"] == call_id:
             return call
     return None
+
+
+def archive_call(call_id):
+    for call in calls:
+        if call["id"] == call_id:
+            if call["is_archived"]:
+                return f"Call with ID:{call_id} is already archived!"
+            else:
+                call["is_archived"] = True
+                return f"Call with ID:{call_id} has been archived!"
+    return f"Call with ID:{call_id} is not exist!"
