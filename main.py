c3090b426db24dcaeacbb353d0fa905b962c5d27
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-09 01:35:29 +0300

    Add get call by id function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from src.services.call_services import get_all_calls
-
+from src.services.call_services import (
+    get_all_calls,
+    get_call_by_id,
+)
 
 print(get_all_calls())
+
+print(get_call_by_id("3"))
--- src/services/call_services.py
+++ src/services/call_services.py
@@ -3,3 +3,10 @@ from src.data.seed_data import calls
 
 def get_all_calls():
     return [call for call in calls if not call["is_archived"]]
+
+
+def get_call_by_id(call_id):
+    for call in calls:
+        if call["id"] == call_id:
+            return call
+    return None
