974b336d31715110cf5ffcd97c9113590a673734
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-11 04:21:27 +0300

    Reorder function in a way they all run

--- main.py
+++ main.py
@@ -15,14 +15,6 @@ print(get_non_archived_calls())
 
 print(get_call_by_id("3"))
 
-print(archive_call("3"))
-
-print(unarchive_call("10"))
-
-print(delete_call("1"))
-
-print(filter_calls("not_archived"))
-
 print(
     add_note(
         "1",
@@ -33,3 +25,11 @@ print(
         },
     )
 )
+
+print(archive_call("3"))
+
+print(unarchive_call("10"))
+
+print(filter_calls("not_archived"))
+
+print(delete_call("1"))
