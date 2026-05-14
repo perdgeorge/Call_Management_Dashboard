ca7b633860a6540a8634bb13eea154e5da74ea56
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-14 16:35:34 +0300

    Add call model in call services

--- src/services/call_services.py
+++ src/services/call_services.py
@@ -1,20 +1,21 @@
 from src.data.seed_data import calls
+from src.models.call import Call, Note
 
 
-def get_all_calls() -> list[dict]:
+def get_all_calls() -> list[Call]:
     for call in calls:
         validate_call(call)
     return calls
 
 
-def get_non_archived_calls() -> list[dict]:
+def get_non_archived_calls() -> list[Call]:
     calls_numbers = [call for call in calls if not call["is_archived"]]
     for number in calls_numbers:
         validate_call(number)
     return calls_numbers
 
 
-def get_call_by_id(call_id: str) -> dict | str:
+def get_call_by_id(call_id: int) -> Call | str:
     for call in calls:
         if call["id"] == call_id:
             validate_call(call)
@@ -22,7 +23,7 @@ def get_call_by_id(call_id: str) -> dict | str:
     return f"Call with ID:{call_id} is not exist!"
 
 
-def add_note(call_id: str, note: dict) -> dict | str:
+def add_note(call_id: int, note: Note) -> Call | str:
     for call in calls:
         if call["id"] == call_id:
             validate_call(call)
@@ -35,7 +36,7 @@ def add_note(call_id: str, note: dict) -> dict | str:
     return f"Call with ID:{call_id} is not exist!"
 
 
-def archive_call(call_id: str) -> str:
+def archive_call(call_id: int) -> str:
     for call in calls:
         if call["id"] == call_id:
             validate_call(call)
@@ -47,7 +48,7 @@ def archive_call(call_id: str) -> str:
     return f"Call with ID:{call_id} is not exist!"
 
 
-def unarchive_call(call_id: str) -> str:
+def unarchive_call(call_id: int) -> str:
     for call in calls:
         if call["id"] == call_id:
             validate_call(call)
@@ -59,7 +60,7 @@ def unarchive_call(call_id: str) -> str:
     return f"Call with ID:{call_id} is not exist!"
 
 
-def filter_calls(call_filter: str) -> list[dict] | None:
+def filter_calls(call_filter: str) -> list[Call] | str:
     all_calls = get_all_calls()
     if call_filter in ("answered", "voicemail", "missed"):
         return [call for call in all_calls if call["call_type"] == call_filter]
@@ -73,7 +74,7 @@ def filter_calls(call_filter: str) -> list[dict] | None:
         return "Error: Invalid filter! Filter must be either answered, voicemail, missed, outbound, inbound, archived or not_archived"
 
 
-def delete_call(call_id: str) -> str:
+def delete_call(call_id: int) -> str:
     for call in calls:
         if call["id"] == call_id:
             validate_call(call)
@@ -82,7 +83,7 @@ def delete_call(call_id: str) -> str:
     return f"Call with ID:{call_id} is not exist!"
 
 
-def validate_call(call: dict):
+def validate_call(call: Call) -> str | None:
     caller = call["from_number"].replace(" ", "")
     receiver = call["to_number"].replace(" ", "")
     if call["direction"] not in ("inbound", "outbound"):
