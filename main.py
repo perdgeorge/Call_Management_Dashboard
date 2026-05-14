ed2bdf7a699b0d4138d53d171093637dd9d19942
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-14 23:17:50 +0300

    Add filter_calls endpoint instead of print(filter_calls)

--- main.py
+++ main.py
@@ -28,6 +28,11 @@ async def call_by_id(call_id: str):
     return get_call_by_id(call_id)
 
 
+@app.get("/Filter_Calls/{call_filter}")
+async def filter_call(call_filter: str):
+    return filter_calls(call_filter)
+
+
 @app.patch("/Archive_Call/{call_id}")
 async def archive(call_id: str):
     return archive_call(call_id)
