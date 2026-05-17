4441566b5972ec3ce803fbde8dd96d4065ce3337
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-18 02:18:27 +0300

    Add pydantic schemas for Call

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -1,4 +1,7 @@
+from datetime import datetime
+from typing import List
 from pydantic import BaseModel, ConfigDict, Field
+from src.core.enums import CallDirection, CallType
 
 
 class BaseSchema(BaseModel):
@@ -15,4 +18,43 @@ class GetNoteSchema(NoteSchema):
 
 
 class CreateNoteSchema(NoteSchema):
+    pass
+
+
+class GetCallSchema(BaseSchema):
+    id: int = Field(..., examples=1)
+    direction: CallDirection = Field(..., examples="inbound")
+    from_number: str = Field(..., max_length=15, examples="+30 123 4567890")
+    to_number: str = Field(..., max_length=15, examples="+30 098 7654321")
+    call_type: CallType = Field(..., examples="answered")
+    duration: int = Field(..., examples=120)
+    created_at: datetime = Field(..., examples="2023-01-01T00:00:00")
+    is_archived: bool = Field(..., examples=False)
+    notes: List[GetNoteSchema] | None = Field(
+        examples=[
+            {
+                "id": 1,
+                "call_id": 1,
+                "content": "Customer left a message about their invoice",
+            }
+        ],
+        default=None,
+    )
+
+
+class UpdateCallSchema(BaseSchema):
+    is_archived: bool = Field(..., examples=False)
+    notes: List[GetNoteSchema] | None = Field(
+        examples=[
+            {
+                "id": 1,
+                "call_id": 1,
+                "content": "Customer left a message about their invoice",
+            }
+        ],
+        default=None,
+    )
+
+
+class DeleteCallSchema(GetCallSchema):
     pass
