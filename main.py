5a916f8fa4983fd1010ce9d5af040fd42b284507
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-18 02:57:57 +0300

    Import schemas into my endpoints

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from src.models.call import Note
 from src.services.call_services import (
     get_all_calls,
     get_non_archived_calls,
@@ -11,28 +10,33 @@ from src.services.call_services import (
     add_note_by_id,
 )
 from src.core.exception_handlers import register_exception_handlers
+from src.core.schemas import (
+    GetCallSchema,
+    CreateNoteSchema,
+    DeleteCallSchema,
+)
 
 app = FastAPI()
 register_exception_handlers(app)
 
 
-@app.get("/Calls")
+@app.get("/Calls", response_model=list[GetCallSchema])
 async def all_calls():
     return get_all_calls()
 
 
-@app.get("/Non_Archived_Calls")
+@app.get("/Non_Archived_Calls", response_model=list[GetCallSchema])
 async def non_archived_calls():
     return get_non_archived_calls()
 
 
-@app.get("/Calls/{call_id}")
+@app.get("/Calls/{call_id}", response_model=GetCallSchema)
 async def call_by_id(call_id: int):
     return get_call_by_id(call_id)
 
 
-@app.get("/Calls/{call_filter}/Filter")
-async def filter_call(call_filter: int):
+@app.get("/Calls/{call_filter}/Filter", response_model=list[GetCallSchema])
+async def filter_call(call_filter: str):
     return filter_calls(call_filter)
 
 
@@ -47,11 +51,11 @@ async def unarchive(call_id: int):
 
 
 @app.patch("/Calls/{call_id}/Note")
-async def add_note(call_id: int, note: Note):
+async def add_note(call_id: int, note: CreateNoteSchema):
     return add_note_by_id(call_id, note)
 
 
-@app.delete("/Calls/{call_id}")
+@app.delete("/Calls/{call_id}", response_model=DeleteCallSchema)
 async def delete(call_id: int):
     return delete_call_by_id(call_id)
 
