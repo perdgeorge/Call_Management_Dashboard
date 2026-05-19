0ef1c0f6fcbf3f0ad51734b6b705afe09efffdf5
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-19 16:04:43 +0300

    Update schemas and import pattern in Field for numbers validation

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import List
 from pydantic import BaseModel, ConfigDict, Field
+
 from src.core.enums import CallDirection, CallType
 
 
@@ -21,16 +22,25 @@ class CreateNoteSchema(NoteSchema):
     pass
 
 
-class GetCallSchema(BaseSchema):
-    id: int = Field(..., examples=[1])
+class CallSchema(BaseSchema):
     direction: CallDirection = Field(..., examples=["inbound"])
-    from_number: str = Field(..., max_length=15, examples=["+30 123 4567890"])
-    to_number: str = Field(..., max_length=15, examples=["+30 098 7654321"])
+    from_number: str = Field(
+        ...,
+        pattern=r"^\+\d{2}\s\d{3}\s\d{7}$",
+        max_length=15,
+        examples=["+30 123 4567890"],
+    )
+    to_number: str = Field(
+        ...,
+        pattern=r"^\+\d{2}\s\d{3}\s\d{7}$",
+        max_length=15,
+        examples=["+30 098 7654321"],
+    )
     call_type: CallType = Field(..., examples=["answered"])
     duration: int = Field(..., examples=[120])
     created_at: datetime = Field(..., examples=["2023-01-01T00:00:00"])
     is_archived: bool = Field(..., examples=[False])
-    notes: List[GetNoteSchema] | None = Field(
+    notes: List[CreateNoteSchema] | None = Field(
         examples=[
             {
                 "id": 1,
@@ -42,9 +52,17 @@ class GetCallSchema(BaseSchema):
     )
 
 
+class GetCallSchema(CallSchema):
+    id: int = Field(..., examples=[1])
+
+
+class CreateCallSchema(CallSchema):
+    pass
+
+
 class UpdateCallSchema(BaseSchema):
     is_archived: bool = Field(..., examples=[False])
-    notes: List[GetNoteSchema] | None = Field(
+    notes: List[CreateNoteSchema] | None = Field(
         examples=[
             {
                 "id": 1,
