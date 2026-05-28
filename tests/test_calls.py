9197db621c84cd4e4198914d6c3b8e56ddb57cb9
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-05-29 00:30:15 +0300

    Add test_get_call_by_id

--- tests/test_calls.py
+++ tests/test_calls.py
@@ -9,3 +9,9 @@ def test_get_all_calls():
     response = client.get("/calls")
     assert response.status_code == 200
     assert len(response.json()) > 0
+
+
+def test_get_call_by_id():
+    response = client.get("/calls/2")
+    assert response.status_code == 200
+    assert response.json()
