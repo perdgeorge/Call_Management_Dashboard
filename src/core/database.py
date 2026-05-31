7852e53247543de77a92b39c297890d7004a1ae0
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-06-01 02:28:18 +0300

    Import TimestampMixing class for created_at attribute

--- src/core/database.py
+++ src/core/database.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from sqlalchemy import DateTime, create_engine, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
 
 load_dotenv()
 
@@ -17,6 +18,12 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
     pass
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
 
 
 def get_db():
