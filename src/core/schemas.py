56d9c94b288c21fb64bec476eb2c12532a367333
George Perdikaris <83364418+perdgeorge@users.noreply.github.com>
2026-06-01 04:45:18 +0300

    Update GetCallSchema to reorder fields

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -47,8 +47,24 @@ class CallSchema(BaseSchema):
     )
 
 
-class GetCallSchema(CallSchema):
+class GetCallSchema(BaseSchema):
     id: int = Field(..., examples=[1])
+    direction: CallDirection = Field(..., examples=["inbound"])
+    from_number: str = Field(
+        ...,
+        pattern=r"^\+\d{2}\s\d{3}\s\d{7}$",
+        max_length=15,
+        examples=["+30 123 4567890"],
+    )
+    to_number: str = Field(
+        ...,
+        pattern=r"^\+\d{2}\s\d{3}\s\d{7}$",
+        max_length=15,
+        examples=["+30 098 7654321"],
+    )
+    call_type: CallType = Field(..., examples=["answered"])
+    duration: int = Field(..., examples=[120])
+    is_archived: bool = Field(..., examples=[False])
     created_at: datetime = Field(..., examples=["2023-01-01T00:00:00"])
     notes: List[GetNoteSchema] | None = None
 
